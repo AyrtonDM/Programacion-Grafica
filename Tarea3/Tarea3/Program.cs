ad48c157e9cab13690d5ebe568578f2ca4e3edb3
AyrtonDM <147714764+AyrtonDM@users.noreply.github.com>
2024-08-31 17:01:45 -0400

    Add files via upload

---
+++ Tarea3/Tarea3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using OpenTK;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tarea3;
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        Graficador game = new Graficador();
+        game.Run();
+    }
+}
+
