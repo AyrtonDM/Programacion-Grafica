12f88dfc6b79bd1aa9b512fce85157979ff78cd8
AyrtonDM <147714764+AyrtonDM@users.noreply.github.com>
2024-08-31 16:58:04 -0400

    Add files via upload

---
+++ Tarea3/Tarea3/Tarea3/Objeto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+using OpenTK.Graphics.OpenGL;
+using System.Timers;
+using Microsoft.VisualBasic;
+
+namespace Tarea3
+{
+    public class Objeto
+    {
+        Punto centro;
+        Dictionary<string, Parte> partes;
+
+        public Objeto()
+        {
+            centro = new Punto();
+            partes = new Dictionary<string, Parte>();
+        }
+
+        public Objeto(Dictionary<string, Parte> parte, Punto np)
+        {
+            partes = parte;
+            centro = np;
+        }
+
+        public Objeto(Punto nc)
+        {
+            centro = nc;
+            partes = new Dictionary<string, Parte>();
+        }
+
+        public void agregar(string clave, Parte p)
+        {
+            p.centro.X = centro.X + p.centro.X;
+            p.centro.Y = centro.Y + p.centro.Y;
+            p.centro.Z = centro.Z + p.centro.Z;
+            partes.Add(clave, p);
+        }
+
+        public void remover(string clave)
+        {
+            partes.Remove(clave);
+        }
+
+        public void Dibujar()
+        {
+            foreach (Parte valor in partes.Values)
+            {
+                valor.Dibujar();
+            }
+        }
+    }
+}
---
+++ Tarea3/Tarea3/Tarea3/Punto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea3
+{
+    public class Punto
+    {
+        public float X;
+        public float Y;
+        public float Z;
+
+        public Punto()
+        {
+            X = Y = Z = 0.0f;
+        }
+
+        public Punto(float x, float y, float z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public void Escalar(float valor)
+        {
+            X = valor*X;
+            Y = valor*Y;
+            Z = valor*Z;
+        }
+
+        public void Escalar(float x,float y, float z)
+        {
+            X = x * X;
+            Y = y * Y;
+            Z = z * Z;
+        }
+    }
+}
